42c900f1fa37d83b55f4fb6af78f067be847b93f
Jakub Kraus <poczta.jakub1@gmail.com>
2020-02-15 22:20:11 +0100

    Updater for variables

---
+++ js/main.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  updater();
+};
+
+function updater() {
+  const inputs = document.querySelectorAll(".controls input");
+
+  function handleUpdate() {
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(
+      `--${this.name}`,
+      this.value + suffix
+    );
+  }
+
+  inputs.forEach(input => input.addEventListener("change", handleUpdate));
+  inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+}
